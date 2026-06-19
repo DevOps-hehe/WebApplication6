ad11ef996e1ef2487a499aa64f97c0779b6b024a
Alakh Yadav <stu.alakh@gmail.com>
2026-06-20 02:29:07 +0530

    made changes

--- TestProject1/webtest.cs
+++ TestProject1/webtest.cs
@@ -1,3 +1,5 @@
+using WebApplication6.NewFolder;
+
 namespace TestProject1
 {
     public class webtest
@@ -9,6 +11,22 @@ namespace TestProject1
             bool result = false;
             if (i == 1) result = true;
             Assert.True(result, "value shhould be equal to 1");
+        }
+
+
+        [Fact]
+
+        public void CheckAddFunction()
+        {
+            Functions func = new Functions();
+            int y = func.Add(3, 2);
+
+            bool result = false;
+
+            if (y == 5) result = true;
+
+            Assert.True(result, "value should be equal to 5");
+
         }
     }
 }
---
+++ WebApplication6/NewFolder/Functions.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication6.NewFolder
+{
+    public class Functions
+    {
+        public LazyInitializer Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
